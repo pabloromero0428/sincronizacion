7de99ac6581252538e6bd8d105388a629c439a4f
pabloromero0428 <pablo.romero2840@gmail.com>
2020-04-26 16:15:06 -0500

    punto 3 a medias

---
+++ PARTE 1 Solución/P3/test_precise_counter.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "counter.h"
+
+
+/* structs */
+void* contador(void *);
+
+
+/* start_routine header */
+counter_t counter;
+int MAXCNT;
+int NUMTHREADS;
+
+/* Global variables */
+pthread_mutex_t lock; 
+
+
+
+
+
+int main(int argc, char *argv[]) { 
+
+    /* get the command's line parameters */
+    pthread_mutex_init(&lock, NULL);
+    MAXCNT = atoi(argv[1]);
+    NUMTHREADS = atoi(argv[2]);
+
+
+    /* Declaration of struct timeval variables */
+    struct timeval tinicial, tfinal;
+    long timeFull;
+
+
+    /* Initializing conter */
+    init(&counter);
+
+
+    /* Threads handlers */
+    pthread_t thr[NUMTHREADS];
+
+
+    /* Time starts counting */
+    gettimeofday(&tinicial, NULL);
+
+
+    /* Creating a Threads */
+    for(int i = 0; i < NUMTHREADS; i++){
+       pthread_create(&thr[i], NULL, &contador,NULL);
+    }
+   
+
+    /* Threads joins */
+    for (int i = 0; i < NUMTHREADS; i++){
+        pthread_join(thr[i],NULL);
+    }
+
+
+    /* Time stops counting here */
+    
+    int fin_counter = get(&counter);
+
+
+    /* get the end time */
+     gettimeofday(&tfinal, NULL);
+    
+
+    /* get the elapset time (end_time - start_time) */
+     timeFull = (tfinal.tv_usec - tinicial.tv_usec)/1000.0;
+
+
+    /* print the results (number threads employed, counter value, elasep time) */
+    printf("Numero de hilos corridos: %d | Resultado del contador: %d | tiempo transcurrido: %ld milisegundos \n",NUMTHREADS, fin_counter, timeFull);
+
+
+    return 0;
+}
+
+/* start_routine definition */
+void* contador( void* unused ){
+    pthread_mutex_lock(&lock);
+    int contador_concurente = get(&counter);
+    if(contador_concurente < MAXCNT){
+       for(int i = 0; i < 1000000; i++) {
+            increment(&counter);
+        }
+    }
+    pthread_mutex_unlock(&lock);
+};
